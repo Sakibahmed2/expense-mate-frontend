01d7884571094fb62b8f8a2a44b4e5b63e8b62a1
unknown <mauijaloskor@gmail.com>
2025-01-07 11:58:29 +0600

    frontend all done

---
+++ src/app/summary/page.jsx
@@ -0,0 +1,12 @@
+import Summary from "@/components/pages/Summary.jsx/Summary";
+import React from "react";
+
+const SummaryPage = () => {
+  return (
+    <div>
+      <Summary />
+    </div>
+  );
+};
+
+export default SummaryPage;
--- src/components/pages/Summary.jsx/Summary.jsx
+++ src/components/pages/Summary.jsx/Summary.jsx
@@ -1,7 +1,74 @@
+"use client";
+
 import React from "react";
+import { useSelector } from "react-redux";
+import "@/styles/summary.css";
 
 const Summary = () => {
-  return <div>Summary</div>;
+  const expenses = useSelector((state) => state.expenses.expenses);
+
+  console.log(expenses);
+
+  // Get unique dates from expenses
+  const uniqueDates = [
+    ...new Set(
+      expenses.map((expense) => {
+        return new Date(expense.date).toLocaleDateString();
+      })
+    ),
+  ];
+
+  const categories = [
+    "healthCare",
+    "utility",
+    "transport",
+    "miscellaneous",
+    "groceries",
+    "charity",
+  ];
+
+  const generateTable = () => {
+    return uniqueDates.map((date) => {
+      const row = { date, categories: {} };
+
+      expenses.forEach((expense) => {
+        const expenseDate = new Date(expense.date).toLocaleDateString();
+        if (expenseDate === date) {
+          row.categories[expense.category] = expense.amount;
+        }
+      });
+
+      return row;
+    });
+  };
+
+  const tableData = generateTable();
+
+  return (
+    <div className="table-container">
+      <table border={3} className="styled-table">
+        <caption className="table-caption">Summary of your expenses</caption>
+        <thead>
+          <tr>
+            <th>Date</th>
+            {categories.map((category) => (
+              <th key={category}>{category}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((row) => (
+            <tr key={row.date}>
+              <td>{row.date}</td>
+              {categories.map((category) => (
+                <td key={category}>{row.categories[category] || ""}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Summary;
